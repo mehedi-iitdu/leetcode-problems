2f8ebbecfd1d079db14d810a20b468c3b05def01
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-10 23:09:48 +0600

    Time: 149 ms (91.37%), Space: 52.8 MB (55.39%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -8,7 +8,7 @@ var threeSum = function(nums) {
     let n = nums.length
     
     for(let i = 0; i < n; i++){
-        if(i > 0 && nums[i] == nums[i-1]){
+        if(i > 0 && nums[i] === nums[i-1]){
             continue;   
         }
         let left  = i + 1;
@@ -16,18 +16,20 @@ var threeSum = function(nums) {
         
         while (left < right){
             let sum = nums[i] + nums[left] + nums[right]
-            if (sum == 0){
+            if (sum === 0){
                 result.push([nums[i], nums[left], nums[right]])
                 left++
-                while(nums[left] == nums[left-1] && left < right){
+                while(nums[left] === nums[left-1] && left < right){
                     left++
                 }
             }
-            else if(sum < 0){
-                left++
-            }
-            else{
-                right--
+            else {
+                if(sum < 0){
+                    left++
+                }
+                else{
+                    right--
+                }
             }
         }
     }
