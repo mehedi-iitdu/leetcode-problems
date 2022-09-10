91f0714e0189550f2f4edbcf27998b9ef4c58e78
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-10 23:03:24 +0600

    Time: 150 ms (91.03%), Space: 53 MB (44.02%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,66 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    // sort the array
-    
     nums.sort((a,b)=>a-b);
+    let result = []
     
-    let n = nums.length;
-    
-    let result = [];
-    
-   
-    for(let i=0;i<n;i++){
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] == nums[i-1]){
+            continue;   
+        }
+        let left  = i + 1;
+        let right = nums.length - 1
         
-        // skip the duplicate tuple elemnts
-        
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        
-        // maintain two pointers
-        
-        let left = i+1;
-        
-        let right = n-1;
-        
-        
-        while(left<right){
-            
-            // triplet sum
-            
-            let sum = nums[left] + nums[right] +nums[i] ;
-            
-            // push the triplet to result when sum is 0
-            
-            if(sum==0){
-                result.push([nums[i] ,nums[left],nums[right]]);
-                left++;
-                
-                // skip the duplicate elements
-                
-                // #[-2,-2,0,0,2,2]
-                
-                while(left<right && nums[left]==nums[left-1]) left++;
-
+        while (left < right){
+            let sum = nums[i] + nums[left] + nums[right]
+            if (sum > 0){
+                right--
+            }
+            else if(sum < 0){
+                left++
             }
             else{
-                
-                // if sum is greater than 0 , move the right pointer
-                // as the array is sorted in increasing order
-                
-                 if(sum>0){
-                    right--;
-                    
+                result.push([nums[i], nums[left], nums[right]])
+                left++
+                while(nums[left] == nums[left-1] && left < right){
+                    left++
                 }
-                
-                // move left pointer for negative sum
-                
-                else left++;
             }
         }
-    
     }
     
-    // return result
-
-    return result;
+    return result
 };
