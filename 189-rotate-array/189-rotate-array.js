7daa4665a55f8358280c33e913634e627200589c
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-14 23:35:23 +0600

    Time: 168 ms (28.24%), Space: 53.4 MB (14.50%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -5,7 +5,7 @@
  */
 var rotate = function(nums, k) {
     k = k % nums.length
-    nums.reverse()
+    reverseArray(nums, 0, nums.length - 1)
     reverseArray(nums, 0, k-1)
     reverseArray(nums, k, nums.length - 1)
 };
