476cae07550981a14bbaf2a1d1f1f8f0980f3a9e
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-15 16:24:14 +0600

    Time: 164 ms (5.59%), Space: 48.6 MB (45.26%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let stringArray = s.split(" ");
+    let newStringArray = [];
+    let temp;
+    let element;
+
+    for (element of stringArray){
+        let left = 0;
+        let words = element.split("");
+        let right = words.length - 1;
+
+        while( left < right){
+            let temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+        newStringArray.push(words.join(''))
+    }
+   return newStringArray.join(" ");
+};
