a096574b33c62360f62a2566e520cc1bdd12ed60
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-18 13:57:41 +0600

    Time: 135 ms (25.54%), Space: 44.4 MB (79.15%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,5 +4,15 @@
  */
 var isPalindrome = function(s) {
     s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    return s === s.split("").reverse().join("")
+    
+    let left = 0
+    let right = s.length - 1
+    
+    while(left < right) {
+        if(s[left] != s[right]) return false
+        left++
+        right--
+    }
+    
+    return true
 };
