eb1d03c0952bb13cafb9f08d56c9ed474e91ace7
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-18 13:48:35 +0600

    Time: 130 ms (42.24%), Space: 43 MB (99.35%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -8,15 +8,9 @@ var topKFrequent = function(nums, k) {
     let result = []
     
     for(let i = 0; i < nums.length; i++){
-        if(numMap[nums[i]]){
-            numMap[nums[i]]++
-        }
-        else{
-            numMap[nums[i]] = 1
-        }
+        numMap[nums[i]] = nums[i] in numMap ? numMap[nums[i]] + 1 : 1
     }
     
-    // convert map of numbers to array of nums based on frequency 
     numMap = Object.keys(numMap).sort((a,b) => numMap[b] - numMap[a])
     
     for(let i = 0; i < k; i++){
