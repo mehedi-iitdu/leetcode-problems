295aa71e85bc0e442469f031dc51152e92083a42
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-19 22:23:59 +0600

    Time: 96 ms (53.60%), Space: 42.3 MB (85.87%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -9,10 +9,8 @@ var intersection = function(nums1, nums2) {
     let maxLengthArr = nums1.length >= nums2.length ? nums1 : nums2
         
     for(let i = 0; i < minLengthArr.length; i++){
-        if(maxLengthArr.indexOf(minLengthArr[i]) >= 0) {
-            if(result.indexOf(minLengthArr[i]) < 0){
-                result.push(minLengthArr[i])
-            }
+        if(maxLengthArr.indexOf(minLengthArr[i]) >= 0 && result.indexOf(minLengthArr[i]) < 0) {
+            result.push(minLengthArr[i])
         }
     }
     return result
