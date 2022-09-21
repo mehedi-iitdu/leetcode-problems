73206d0a91d38faebb4b7a15a4077717f1f7f8e8
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-21 19:07:18 +0600

    Time: 117 ms (10.11%), Space: 41.7 MB (88.84%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -6,7 +6,7 @@ var numberOfSteps = function(num) {
     let steps = 0
     
     while(num > 0){
-        num%2 === 0 ? num = Math.floor(num/2) : num = num -1
+        num%2 === 0 ? num /=2 : num-=1
         steps++
     }
     
