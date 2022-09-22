95cc5af58d0f7b37c6ce836e806262f9233849bd
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-22 18:57:06 +0600

    Time: 112 ms (36.37%), Space: 43.1 MB (86.26%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let chars = {}
+    
+    for(let i = 0; i < s.length; i++){
+        s[i] in chars ? chars[s[i]]++ : chars[s[i]] = 1
+    }
+    
+    let maxLength = 0
+    let oddCharAdded = false
+    
+    for(let item in chars){
+        if(chars[item] % 2 === 0) maxLength += chars[item]
+        else {
+            if(!oddCharAdded) {
+                maxLength += chars[item]
+                oddCharAdded = true
+            }
+            else maxLength += Math.floor(chars[item] - 1)
+        }
+    }
+    
+    return maxLength
+};
