3d22c1797ead470eb3d868b5294fb90506abb529
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-22 18:25:16 +0600

    Time: 54 ms (99.75%), Space: 44.3 MB (23.85%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -6,10 +6,11 @@ var minCostClimbingStairs = function(cost) {
     dp = []
     dp[0] = cost[0]
     dp[1] = cost[1]
+    let length = cost.length
     
-    for(let i = 2; i < cost.length; i++){
+    for(let i = 2; i < length; i++){
         dp[i] = Math.min(dp[i-2] + cost[i], dp[i-1] + cost[i])
     }
     
-    return Math.min(dp[cost.length-2], dp[cost.length-1])
+    return Math.min(dp[length-2], dp[length-1])
 };
