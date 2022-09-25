25696121ee583fa19692083c3e9de0cce752beaf
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-25 12:35:35 +0600

    Time: 109 ms (60.12%), Space: 43.4 MB (78.69%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -5,7 +5,7 @@
 var singleNumber = function(nums) {
     let result = 0
     for(let i = 0; i < nums.length; i++){
-        result = nums[i] ^ result
+        result = result ^ nums[i]
     }
     
     return result
