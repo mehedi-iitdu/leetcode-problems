c94364a00596772ea1b8ee420487386587cb73b2
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-28 18:05:18 +0600

    Time: 757 ms (43.82%), Space: 119.5 MB (35.13%) - LeetHub

---
+++ 204-count-primes/204-count-primes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    let isPrime = new Array(n).fill(true)
+    isPrime[0] = isPrime[1] = false
+    
+    for(let i=2; i < n/2; i++){
+        if(!isPrime[i]) continue
+        
+        for(let j=i*2; j < n; j+=i){ 
+            isPrime[j]=false;
+        }
+    }
+    
+    return isPrime.filter(item=>item==true).length 
+};
