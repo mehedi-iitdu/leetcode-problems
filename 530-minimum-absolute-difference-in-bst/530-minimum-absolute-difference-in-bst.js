9671e2ff056b3277b871c70acae639a609a96577
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-30 00:21:18 +0600

    Time: 412 ms (5.52%), Space: 48.1 MB (49.39%) - LeetHub

---
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    
+    let globalMin = Number.MAX_SAFE_INTEGER
+    
+    const DFS = (root, previousNodeVal, path) => {
+        if (!root) return 
+        
+        //calculate abs difference with previous nodes
+        for(let i = 0; i < path.length; i++){
+            globalMin = Math.min(globalMin, Math.abs(root.val - path[i]))
+        }
+        
+        //push current node into path
+        path.push(root.val)
+        
+        DFS(root.left, root.val, path)
+        DFS(root.right, root.val, path)
+    }
+    
+    DFS(root, globalMin, path=[])
+    
+    return globalMin
+};
